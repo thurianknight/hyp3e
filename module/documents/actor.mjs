9a575a09a8f0c8192d09d20a05880cfa24227533
shermad1 <dsherman@colasiss.com>
2024-12-08 21:10:29 +0100

    Issue #52: Fix token default settings for PCs

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -53,13 +53,6 @@ export class Hyp3eActor extends Actor {
     // Make modifications to data here. For example:
     const systemData = actorData.system;
 
-    // Loop through attribute scores, and add their modifiers to our sheet output.
-    for (let [key, attribute] of Object.entries(systemData.attributes)) {
-      // Example of how to calculate the modifier using d20 rules.
-      //attribute.mod = Math.floor((attribute.value - 10) / 2);
-      // NOTHING TO DO HERE...
-    }
-
     // Calculated fields go here...
 
     // Add base class, used for crit hit & crit miss tables
@@ -208,6 +201,8 @@ export class Hyp3eActor extends Actor {
     if (data.type == "character") {
       this.updateSource({
         "prototypeToken.actorLink": true,
+        "prototypeToken.sight.enabled": true,
+        "prototypeToken.disposition": 0
       });
     }
   }
