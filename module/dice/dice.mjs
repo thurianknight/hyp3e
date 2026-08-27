56d5e299cd1dab0ce593e5b8f4200d56a3ddd55a
thurianknight <thurianknight@gmail.com>
2026-08-27 10:51:40 +0200

    Work on #345

--- module/dice/dice.mjs
+++ module/dice/dice.mjs
@@ -33,7 +33,7 @@ export class Hyp3eDice {
     const itemAtkMod = parseInt(itemData.atkMod) > 0 ? `+${parseInt(itemData.atkMod)}` : `${parseInt(itemData.atkMod)}`;
 
     // Check if the weapon attack has Master or Grandmaster flags set
-    const actorMastery = actorData?.weaponProficiencies.find(w => w.weapon == itemData.baseWeapon).mastery ?? 0;
+    const actorMastery = actorData?.weaponProficiencies.find(w => w.weapon == itemData.baseWeapon)?.mastery ?? 0;
     if (itemData.wpnGrandmaster) {
         masteryMod = 2;
     } else if (itemData.wpnMaster) {
