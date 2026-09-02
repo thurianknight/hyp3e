daac1628f322d2633c2163005ce1cffcb75dc32a
thurianknight <thurianknight@gmail.com>
2026-09-02 10:35:32 +0200

    Compendium unpacking script, file cleanup and version update

--- unpack-data.py
+++ unpack-data.py
@@ -1,7 +1,7 @@
 # Command to run: 
 #   source .venv/bin/activate
 #   python3 ./unpack-data.py
-# Description: Unpacks the hyp3e core data into the src folder. The world must not be 
+# Description: Unpacks the hyp3e core data into the compendium-src folder. The world must not be 
 #   open or logged into at the time of running this script.
 
 import json
@@ -12,6 +12,6 @@ subprocess.run(["fvtt", "package", "workon", "hyp3e"])
 compendia = json.load(open('system.json', 'r'))
 for pack in compendia['packs']:
     print (f"Unpacking {pack['name']}...")
-    cmd = ["fvtt", "package", "unpack", pack['name'], "--out", "./compendium-src/" + pack['path'], "--clean"]
+    cmd = ["fvtt", "package", "unpack", pack['name'], "--out", "./compendium-src/" + pack['name'], "--clean"]
     print(cmd)
     subprocess.run(cmd)
